ceb14b9c00ef49f1636312c2dfa5c62808e650cf
Atqiah <124320644+Atqiah@users.noreply.github.com>
2023-02-03 11:03:42 +0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+	<form action="login.php" method="post">
+<h1><center>Informasi</center></h1>
+<img src="tugas.jpeg">
+<p><h4>
+Pameran Indonesia International Motor Show (IIMS) Hybid 2022 diselenggarakan pada 31 Maret sampai 10 April 2022. Ratusan ribu orang mengunjungi pameran otomotif yang digelar di JIExpo, Kemayoran, Jakarta ini.
+</h4></p>
+<h4>IIMS 2022 ini dimeriahkan oleh brand kendaraan roda empat yang diwakili oleh BMW, Chery, DFSK, Honda, Hyundai, Jeep, Mazda, MG, Mitsubishi, Prestige Motorcars yang menampilkan unit Tesla terbaru, Toyota, Suzuki serta Wuling.</h4>
+<h4>Tak cuma kendaraan roda empat, ada juga beberapa merek roda dua yang memeriahkan IIMS Hybrid 2022 seperti Benelli & Keeway, Energica, Gesits, Honda,Italjet, Kawasaki, KTM & Husqvarna, NIU, Royal Alloy, Royal Enfield, Volta, dan Yamaha. Perhimpunan Pengusaha Aksesoris Mobil Indonesia (PAHAMI) serta puluhan brand Aftermarket pun meramaikan pameran otomotif ini.</h4>
+</form>
+<center><nav><a href="login.php">Login</nav></center></a>
+</body>
+</html>
